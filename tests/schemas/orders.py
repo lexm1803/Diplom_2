56f32747f80885cf5e72cf76de4e6e62b8930e22
lexm18 <aleksa9518@mail.ru>
2026-01-15 22:49:44 +0300

    Created orders pydantic model

---
+++ tests/schemas/orders.py
@@ -0,0 +1,41 @@
+from pydantic import BaseModel, ConfigDict, Field, EmailStr
+from typing import Optional
+
+
+class CreateOrderRequestSchema(BaseModel):
+    ingredients: list[str]
+
+class OrderResponseSchema(BaseModel):
+    number: int
+
+class CreateOrderResponseSchema(BaseModel):
+    success: bool
+    name: str
+    order: OrderResponseSchema
+
+class GetUserOrdersResponseSchema(BaseModel):
+    success: bool
+    orders: list[dict]
+    total: int
+    total_today: int=Field(alias='totalToday')
+    model_config = ConfigDict(populate_by_name = True)
+
+class IngredientSchema(BaseModel):
+    id: str=Field(alias='_id')
+    name: str
+    type: str
+    proteins: int
+    fat: int
+    carbohydrates: int
+    calories: int
+    price: int
+    image: str
+    image_mobile: str
+    image_large: str
+    version: Optional[int]=Field(alias='__v', default = None)
+    model_config = ConfigDict(populate_by_name = True)
+
+class IngredientsResponseSchema(BaseModel):
+    success: bool
+    data: list[IngredientSchema]
+    
